d5d18330a03b3bc58d33a5a984307499d4ede43a
CONCRETCOL <administracion@concretoestampadocolombia.com>
2026-05-15 19:17:01 -0500

    fix: csrf https

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 app.config['WTF_CSRF_TIME_LIMIT'] = 3600
+app.config['WTF_CSRF_SSL_STRICT'] = False
+app.config['SESSION_COOKIE_SECURE'] = False
 app.config['PERMANENT_SESSION_LIFETIME'] = 28800  # 8 h
 
 db = SQLAlchemy(app)
